f60e26bd32cabe0fb21bcb784e1ef4263c568ec5
lunaInsidious <itsukitakatsuno@gmail.com>
2025-09-19 02:09:19 +0900

    init

---
+++ manifest.ts
@@ -0,0 +1,44 @@
+import { Manifest } from "deno-slack-sdk/mod.ts";
+import { PlayersDatastore } from "./datastores/players.ts";
+import { MatchesDatastore } from "./datastores/matches.ts";
+import { ContentsDatastore } from "./datastores/contents.ts";
+import { ContentRatingsDatastore } from "./datastores/content_ratings.ts";
+import { RatingWorkflow } from "./workflows/rating_workflow.ts";
+import { RankingsWorkflow } from "./workflows/rankings_workflow.ts";
+import { CollectMatchInfoFunction } from "./functions/collect_match_info.ts";
+import { ProcessMatchFunction } from "./functions/process_match.ts";
+import { GetRankingsFunction } from "./functions/get_rankings.ts";
+import { SelectContentFunction } from "./functions/select_content.ts";
+
+/**
+ * The app manifest contains the app's configuration. This
+ * file defines attributes like app name and description.
+ * https://api.slack.com/automation/manifest
+ */
+export default Manifest({
+  name: "Rating System",
+  description: "Slack連携レーティング計算システム",
+  icon: "assets/card_game.png",
+  workflows: [RatingWorkflow, RankingsWorkflow],
+  functions: [
+    CollectMatchInfoFunction,
+    ProcessMatchFunction,
+    GetRankingsFunction,
+    SelectContentFunction,
+  ],
+  datastores: [PlayersDatastore, MatchesDatastore, ContentsDatastore, ContentRatingsDatastore],
+  outgoingDomains: [],
+  botScopes: [
+    "commands",
+    "chat:write",
+    "chat:write.public",
+    "datastore:read",
+    "datastore:write",
+    "users:read",
+    "users:read.email",
+    "channels:read",
+    "groups:read",
+    "mpim:read",
+    "im:read",
+  ],
+});
